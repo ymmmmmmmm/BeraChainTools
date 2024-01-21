df7d73389be2e9e93f514ec8d450e49c5ea8539f
ym <49154181@qq.com>
2024-01-21 17:49:08 +0800

    修复bug

--- config/contract_config.py
+++ config/contract_config.py
@@ -2,14 +2,20 @@
 # Time     :2024/1/21 12:15
 # Author   :ym
 # File     :contract_config.py
+import os
+
+from dotenv import load_dotenv
 from web3 import Web3
 
 from config.abi_config import bex_abi, erc_20_abi, honey_abi
 from config.address_config import bex_swap_address, honey_swap_address, usdc_address, honey_address
 
-w3 = Web3()
-bex_contract = w3.eth.contract(address=bex_swap_address, abi=bex_abi)
+load_dotenv()
+max_workers = int(os.getenv("MaxWorkers"))
+rpc_url = os.getenv("RPC_URL")
+w3 = Web3(Web3.HTTPProvider(rpc_url))
 
+bex_contract = w3.eth.contract(address=bex_swap_address, abi=bex_abi)
 honey_swap_contract = w3.eth.contract(address=honey_swap_address, abi=honey_abi)
 usdc_contract = w3.eth.contract(address=usdc_address, abi=erc_20_abi)
 honey_contract = w3.eth.contract(address=honey_address, abi=erc_20_abi)
