47ced28b72b97ffa13bbc9423a518b2781adea8a
daniel <daniel@qq.com>
2024-01-25 22:02:14 +0800

    发布版本：v1.25.2024

---
+++ utils/deploy_contract.py
@@ -0,0 +1,38 @@
+import random
+import string
+import time
+
+from eth_account import Account
+from loguru import logger
+from solcx import install_solc
+
+from bera_tools import BeraChainTools
+
+
+def deploy_contract(private_key):
+    for _ in range(10):
+        if deploy_contract_(private_key):
+            return
+
+
+def deploy_contract_(private_key):
+    try:
+        account = Account.from_key(private_key)
+        bera = BeraChainTools(private_key=account.key, rpc_url='https://rpc.ankr.com/berachain_testnet')
+        # 安装0.4.18 版本编译器
+        install_solc('0.4.18')
+        # 读取sol文件
+        with open('./config/WETH.sol', 'r') as f:
+            code = f.read()
+        random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+        code = code.replace('Bera Test Ether', 'Bera Test Ether' + random_string)
+        # 部署合约
+        result = bera.deploy_contract(code, '0.4.18')
+        logger.debug(result)
+        logger.success('部署合约成功')
+        logger.debug('-------------------------------------------------------------------------------------')
+        return True
+    except Exception as e:
+        logger.error(f'部署合约失败，{e}')
+        time.sleep(5)
+    return False
---
+++ utils/honey_interaction_utils.py
@@ -0,0 +1,36 @@
+from eth_account import Account
+from loguru import logger
+
+from bera_tools import BeraChainTools
+from config.address_config import honey_swap_address, usdc_address, honey_address
+
+
+def honey_interacte(private_key):
+    account = Account.from_key(private_key)
+    bera = BeraChainTools(private_key=account.key, rpc_url='https://rpc.ankr.com/berachain_testnet')
+
+    try:
+        # 授权usdc
+        logger.debug('STGUSDC转换HONEY开始')
+        approve_result = bera.approve_token(honey_swap_address, int("0x" + "f" * 64, 16), usdc_address)
+        logger.debug(approve_result)
+
+        # 使用usdc mint honey
+        usdc_balance = bera.usdc_contract.functions.balanceOf(account.address).call()
+        result = bera.honey_mint(int(usdc_balance * 0.5))
+        logger.debug(result)
+        logger.success('STGUSDC转换HONEY成功')
+
+        logger.debug('HONEY转换STGUSDC开始')
+        # 授权honey
+        approve_result = bera.approve_token(honey_swap_address, int("0x" + "f" * 64, 16), honey_address)
+        logger.debug(approve_result)
+        # 赎回
+        honey_balance = bera.honey_contract.functions.balanceOf(account.address).call()
+        result = bera.honey_redeem(int(honey_balance * 0.5))
+        logger.debug(result)
+        logger.success('HONEY转换STGUSDC成功,赎回成功！！！')
+    except Exception as e:
+        logger.error(e)
+
+    logger.debug('-------------------------------------------------------------------------------------')
