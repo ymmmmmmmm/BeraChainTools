1a297abfce0b1d3ddc712accf388db87a4a99842
ymmmmmmmm <51306299+ymmmmmmmm@users.noreply.github.com>
2024-01-30 17:13:20 +0800

    Update batch_claim.py

--- script/batch_claim.py
+++ script/batch_claim.py
@@ -19,9 +19,10 @@ async def get_2captcha_google_token(session: aiohttp.ClientSession) -> Union[boo
     params = {'key': client_key, 'method': 'userrecaptcha', 'version': 'v3', 'action': 'submit', 'min_score': 0.5,
               'googlekey': '6LfOA04pAAAAAL9ttkwIz40hC63_7IsaU2MgcwVH', 'pageurl': 'https://artio.faucet.berachain.com/',
               'json': 1}
-    async with session.get('https://api.ez-captcha.com/createTask', params=params) as response:
+    async with session.get('https://2captcha.com/in.php?', params=params) as response:
         response_json = await response.json()
-        if response_json['errorId'] != 0:
+        # logger.debug(response_json)
+        if response_json['status'] != 1:
             logger.warning(response_json)
             return False
         task_id = response_json['request']
